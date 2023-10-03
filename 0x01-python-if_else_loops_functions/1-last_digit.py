e02510c7707557a1632dc89a29b2c136b1fe5203
fadirafik <fadihadddad11@gmail.com>
2023-10-03 19:53:54 +0300

    this is an automated commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ if last_number == 0:
     string2 = "and is 0"
 elif last_number > 5:
     string2 = "and is greater than 5"
-else :
+else:
     string2 = "and is less than 6 and not 0"
 print(string1 + string2)
