915aee46d2f4c1af3a6977ac29df486b9fa4e86e
fadirafik <fadihadddad11@gmail.com>
2023-10-03 03:18:12 -0700

    this is the first commit from the sandbox

--- 0x01-python_if_else_loops_functions/6-print_comb3.py
+++ 0x01-python_if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,7 @@ for i in range(10):
     for j in range(10):
         if j < i or i == j:
             continue
+        if j == 9 and i == 8:
+            print(f"{i}{j}")
+            continue
         print(f"{i}{j} ,", end="")
