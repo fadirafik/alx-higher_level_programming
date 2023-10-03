e761db8ff9d929bf4418677302006a5d5b0660aa
fadirafik <fadihadddad11@gmail.com>
2023-10-04 01:31:05 +0300

    this is an automated commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,8 @@ for i in range(10):
     for j in range(10):
         if j < i or i == j:
             continue
+        if i == 0 and j == 1:
+            print("{}{}".format(i, j))
         if j == 9 and i == 8:
             print(" {}{}".format(i, j))
             continue
