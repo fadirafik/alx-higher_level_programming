2cb1d0b8559f8d9e2a54c10b05ddf9fa6cf8343f
fadirafik <fadihadddad11@gmail.com>
2023-10-04 02:49:21 +0300

    this is an automated commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    i = 0
-    cpy = ""
-    for s in str:
-        if i != n:
-            cpy += str[i]
-        i += 1
-        return cpy
+    if n >= 0:
+        str = str[:n] + str[n + 1:]
+    return str
