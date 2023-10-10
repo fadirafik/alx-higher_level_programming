dda05cbcfbdb4fa5ddd1d4c8e27627639f94a46c
fadirafik <fadihadddad11@gmail.com>
2023-10-10 16:35:14 +0300

    this is an automated commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-c = add(a, b)
+    c = add(a, b)
 
-print("{:d} + {:d} = {:d}".format(a,b,c))
+    print("{:d} + {:d} = {:d}".format(a,b,c))
