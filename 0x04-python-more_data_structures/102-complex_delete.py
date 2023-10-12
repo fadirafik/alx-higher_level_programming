c00f8db343eeb2b42ee8324bea194730121d6249
fadirafik <fadihadddad11@gmail.com>
2023-10-12 04:18:10 +0300

    this is an automated commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    todel = ""
+    todel = []
     for key, val in a_dictionary.items():
         if val == value:
-            todel = key
-    if todel != "":
-        a_dictionary.pop(todel)
+            todel.append(key)
+    if len(todel) != 0:
+        for i in todel:
+            a_dictionary.pop(todel[todel.index(i)])
     return a_dictionary
