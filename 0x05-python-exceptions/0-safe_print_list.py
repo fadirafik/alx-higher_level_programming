1afe70029a9a473e981421dd75ff310884b66986
fadirafik <fadihadddad11@gmail.com>
2023-10-24 00:26:57 +0300

    this is an automated commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        return (i)
+    else:
+        return (i + 1)
+    finally:
+        print("")
