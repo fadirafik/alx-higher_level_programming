6c50fa08e4aa23ab0b0887ec323b5321f126507c
fadirafik <fadihadddad11@gmail.com>
2023-10-24 00:41:37 +0300

    this is an automated commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-        print("")
-        return (i + 1)
-    except IndexError:
-        print("")
-        return (i)
+    c = 0
+    for i in range(x):
+        try:
+
+                print("{}".format(my_list[i]), end="")
+                c += 1
+        except IndexError:
+            continue
+    print("")
+    return (c)
