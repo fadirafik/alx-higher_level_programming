93a7e55fd3e4ce214c6e3367c969273479372cff
fadirafik <fadihadddad11@gmail.com>
2023-10-25 02:55:58 +0300

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this is a square class to be defined"""
+
+
+class Square:
+    """this is a square class"""
+    def __init__(self, size=0):
+        self.__size = size
+        if isinstance(size, int) is False:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
