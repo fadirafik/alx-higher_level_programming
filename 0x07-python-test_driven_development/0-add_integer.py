c13cf56f3665605af5d374b85f526e4b3ecc1f86
fadirafik <fadihadddad11@gmail.com>
2023-10-30 01:09:14 +0200

    this is an automated commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+"""adds two integers together"""
+def add_integer(a, b=98):
+    """adds two numbers together"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int (b)
