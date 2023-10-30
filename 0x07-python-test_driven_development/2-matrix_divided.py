759fd8669812b54e567495e0046f2c64902b3a50
fadirafik <fadihadddad11@gmail.com>
2023-10-30 03:02:57 +0200

    this is an automated commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+
+
+module for dividing matrices
+
+
+"""
+def matrix_divided(matrix, div):
+    """
+    
+    matrix division bt divisor
+    
+    """
+    j = 0
+    c = 0
+    sizerows = len(matrix[1])
+    newmatrix = []
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    for row in matrix:
+        newlist = []
+        if len(row) != sizerows:
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in row:
+            if not isinstance(i, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            newlist.append(round(i/div, 2))
+            c += 1
+        newmatrix.append(newlist)
+        j += 1
+        c = 0
+    return newmatrix
+            
