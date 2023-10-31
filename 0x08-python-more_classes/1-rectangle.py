47828e03431c555a823977840cb04bc5df264da6
fadirafik <fadihadddad11@gmail.com>
2023-10-31 12:59:02 +0200

    this is an automated commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,7 +8,9 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
-
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
