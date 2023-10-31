1ee5d06d6e473bee83cf57916283e43d69f59cf1
fadirafik <fadihadddad11@gmail.com>
2023-10-31 12:23:06 +0200

    this is an automated commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+the module for rectangle class
+"""
+
+
+class Rectangle:
+    """an empty class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+this is an exercise on classes and instances and attributes
