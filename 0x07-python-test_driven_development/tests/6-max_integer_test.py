941eee7cfb489a4208a87af80590c3a94cbd9a7f
fadirafik <fadihadddad11@gmail.com>
2023-11-01 04:07:07 +0200

    this is an automated commit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
 
 class testint(unittest.TestCase):
 
     def test_max_integer(self):
-        result = max_integer([1,2,3,4,3])
+        result = max_integer([1, 2, 3, 4, 3])
         self.assertEqual(result, 4)
