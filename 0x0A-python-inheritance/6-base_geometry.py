fcadf07331cae3e6d4bf8cd522f0c805d87ffe8f
fadirafik <fadihadddad11@gmail.com>
2023-11-07 02:23:41 +0200

    this is an automated commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+base geometry module
+"""
+
+
+class BaseGeometry:
+    """base geometry class"""
+
+    def area(self):
+        """returns the area of the object"""
+        raise Exception('area() is not defined')
