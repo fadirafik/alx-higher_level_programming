d5a7f7cd78dd8f708e9f42120b1f6c14fb475d39
fadirafik <fadihadddad11@gmail.com>
2023-11-07 02:39:28 +0200

    this is an automated commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+base geometry module
+"""
+
+
+class BaseGeometry:
+    """base geometry class"""
+
+    def area(self):
+        """returns the area of the object"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates the value"""
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
