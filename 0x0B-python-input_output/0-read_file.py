460972da027d573e9df44d9b8acfce70e74d9b10
fadirafik <fadihadddad11@gmail.com>
2023-11-07 20:16:53 +0200

    this is an automated commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""
+read file module
+"""
+
+
+def read_file(filename=""):
+    """prints the contents of a file"""
+    with open(filename, encoding="utf-8") as fle:
+        print(fle.read())
