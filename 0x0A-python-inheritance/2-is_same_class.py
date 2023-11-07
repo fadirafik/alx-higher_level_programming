a9d6c47f0f13a850f210d2b3569381b1d2d7ee2c
fadirafik <fadihadddad11@gmail.com>
2023-11-07 02:01:47 +0200

    this is an automated commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+is same class module
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks if objecr is an instance of a class"""\
+
+    if type(obj) is a_class:
+        return True
+    return False
