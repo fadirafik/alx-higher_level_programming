5442e8f82c86b4733cf5072d0f5d1618f45a90d6
fadirafik <fadihadddad11@gmail.com>
2023-11-08 04:16:45 +0200

    this is an automated commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation """
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload a attributes from json"""
+        for a, b in json:
+            setattr(self, a, b)
