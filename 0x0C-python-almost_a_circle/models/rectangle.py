0e78c24b6cb95d037af91e82671c811f39696505
fadirafik <fadihadddad11@gmail.com>
2023-11-13 20:32:01 +0200

    this is an automated commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """a rectangle class"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """initiation of the class rectangle"""
         self.__width = width
@@ -18,13 +18,13 @@ class Rectangle(Base):
     def width(self):
         """gets the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """sets the value of width"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value <= 0 :
+        if value <= 0:
             raise ValueError('width must be > 0')
         self.__width = value
 
@@ -32,12 +32,12 @@ class Rectangle(Base):
     def height(self):
         """Set/get the height of the Rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError('height must be an integer')
-        if value <= 0 :
+        if value <= 0:
             raise ValueError('height must be > 0')
         self.__height = value
 
@@ -45,12 +45,12 @@ class Rectangle(Base):
     def x(self):
         """sets/gets value of x"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if type(value) is not int:
             raise TypeError('x must be an integer')
-        if value < 0 :
+        if value < 0:
             raise ValueError('x must be >= 0')
         self.__x = value
 
@@ -58,20 +58,19 @@ class Rectangle(Base):
     def y(self):
         """sets/gets the value of y"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if type(value) is not int:
             raise TypeError('y must be an integer')
-        if value < 0 :
+        if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
-    
 
     def area(self):
         self.area = self.__height * self.__width
         return self.area
-    
+
     def display(self):
         if self.__height == 0 or self.__width == 0:
             print("")
@@ -85,7 +84,9 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
-        strin = "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
+        strin = "[{}] ({}) {}/{} - {}/{}".\
+            format(type(self).__name__, self.id, self.__x,
+                   self.__y, self.__width, self.__height)
         return strin
 
     def update(self, *args):
